dac881fa0379c07ef11993f742f06ae18a085390
William Mbotta <sergembotta@mailoo.org>
2023-10-24 03:01:39 +0200

    analytics

--- components/header.js
+++ components/header.js
@@ -6,5 +6,13 @@ import { Context as CourseContext } from "../context/courseInfoContext";
 export default function Header(props) {
   const [{ section, title, icon }] = useContext(HeaderContext);
   const { frontendMastersLink } = useContext(CourseContext);
-  return <header className="navbar"></header>;
+  return (
+    <header className="navbar">
+      <script
+        async
+        src="https://analytics.sepir.cloud/script.js"
+        data-website-id="89cab4bb-221b-47bb-991d-296ac995f56c"
+      ></script>
+    </header>
+  );
 }
